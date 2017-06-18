cade6b23a36b0673b56994c4054ab29f4c32a3ee
Bryan Lee <bryan.lee.jh@gmail.com>
2017-06-18 10:02:19 +0800

    small fixes to UI

--- public/js/main.js
+++ public/js/main.js
@@ -68,7 +68,7 @@ function createNewEvent(lat, lng) {
     dangerLevel: $('#dangerLevel').val()
   }
 
-  var contentString = '<div class="iw-description">Threat:'+ newEvent.name +'</div><div class="iw-address">Location: '+ newEvent.address +'</div><div class="iw-category">Category: '+ newEvent.Category +'</div><div class="iw-dangerLevel">Threat Level: '+ newEvent.dangerLevel +'</div>'
+  var contentString = '<div class="iw-description">Threat: '+ newEvent.name +'</div><div class="iw-address">Location: '+ newEvent.address +'</div><div class="iw-category">Category: '+ newEvent.Category +'</div><div class="iw-dangerLevel">Threat Level: '+ newEvent.dangerLevel +'</div>'
 
   var threatMarker = {
     marker: new google.maps.Marker({
@@ -87,7 +87,10 @@ function createNewEvent(lat, lng) {
 
   activeThreats.push(threatMarker);
 
-
+  $('#description').val('');
+  $('#address').val('');
+  //$('#categories').text('People getting hurt');
+  $('#dangerLevel').val(1);
 
   socket.emit('newEvent', newEvent);
 }
@@ -161,7 +164,6 @@ $(document).ready(function() {
 
   document.getElementById('submit').addEventListener('click', function(e) {
     e.preventDefault();
-
     geocodeAddress(geocoder, map);
 
     console.log("geocoding works");
@@ -177,6 +179,5 @@ $(document).ready(function() {
     // var message = $('#chat input').val();
     // socket.emit('newMessage', message);
     // $('#chat input').val('');
-
 
 });
